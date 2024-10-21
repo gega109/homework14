f61ba769604ba11b80301b199c2564e36980e376
gega109 <bakuravar1@gmail.com>
2024-10-21 20:24:04 +0400

    homework 14

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-let number = 11;
-let square = number ** 2;
-if (square <= 100) {
-  console.log("this number isn't over the hundred");
-} else {
-  console.log("this number is over the hundred");
-}
+// let number = 11;
+// let square = number ** 2;
+// if (square <= 100) {
+//   console.log("this number isn't over the hundred");
+// } else {
+//   console.log("this number is over the hundred");
+// }
 
-let number === 10;
-if (number >= 50 && number <= 100) {
-    console.log("this number is over the 50 and under 100")
-} else {
-    console.log("this nubmer is over the hundred or under the 50")
-}
+// let number === 10;
+// if (number >= 50 && number <= 100) {
+//     console.log("this number is over the 50 and under 100")
+// } else {
+//     console.log("this nubmer is over the hundred or under the 50")
+// }
 
-let number = -1;
-if (number <= 0) {
-    console.log("this number is equal to zero or is under the zero")
-} else {
-    console.log("is above 0")
-}
+// let number = -1;
+// if (number <= 0) {
+//     console.log("this number is equal to zero or is under the zero")
+// } else {
+//     console.log("is above 0")
+// }
 
-let width = 3;
-let length = 5;
-let perimeter = width * length;
-if (perimeter > 50) {
-    console.log("perimeter is above 50")
-} else {
-    console.log("perimeter is under 50")
-}
+// let width = 3;
+// let length = 5;
+// let perimeter = width * length;
+// if (perimeter > 50) {
+//     console.log("perimeter is above 50")
+// } else {
+//     console.log("perimeter is under 50")
+// }
